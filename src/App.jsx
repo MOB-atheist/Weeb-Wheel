6d8d16f8f0197ac5900ae3460edb0db7c53fd8b9
pablo justino <pabskne@gmail.com>
2023-09-10 22:55:49 +0000

    First commit, added Vite and created a simple landing page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="hero-container">
+        <h2 className="hero glitch layers" data-text="Coming Soon"><span>Coming Soon</span></h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
